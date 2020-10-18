35e21687e719d8569c1d39b13e975c8af8c66c26
VictorPuga <victorpugaruiz@live.com.mx>
2020-10-17 18:51:41 -0600

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Victor Manuel Puga Ruiz on 17/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Victor Manuel Puga Ruiz on 17/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
