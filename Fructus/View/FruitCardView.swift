b74efbf4c75ad6e2a99c384e96875036f5ebe89c
VictorPuga <victorpugaruiz@live.com.mx>
2020-10-18 19:09:43 -0600

    Data and data model

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct FruitCardView: View {
     // MARK: - Propperties
+    
+    var fruit: Fruit
+    
     @State private var isAnimating: Bool = false
     
     // MARK: - Body
+    
     var body: some View {
         ZStack {
             VStack(spacing: 20) {
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(
@@ -25,7 +29,7 @@ struct FruitCardView: View {
                         y: 8
                     )
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
@@ -35,7 +39,7 @@ struct FruitCardView: View {
                         x: 2,
                         y: 2
                     )
-                Text("Blueberries are sweet, nutritious, and widely popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
@@ -57,10 +61,7 @@ struct FruitCardView: View {
         )
         .background(LinearGradient(
                         gradient: Gradient(
-                            colors: [
-                                Color("ColorBlueberryLight"),
-                                Color("ColorBlueberryDark"),
-                            ]
+                            colors: fruit.gradientColors
                         ),
                         startPoint: .top,
                         endPoint: .bottom)
@@ -73,7 +74,9 @@ struct FruitCardView: View {
 // MARK: - Preview
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[2])
+            .previewLayout(.fixed(width: 320, height: 640))
+        FruitCardView(fruit: fruitsData[1])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
